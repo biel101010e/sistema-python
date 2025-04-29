60a87b714f3dedd39f3b2b407767fe14090775dc
biel101010e <00001125278092sp@al.educacao.sp.gov.br>
2025-04-29 10:18:56 -0300

    modifiquei o sistema fazendo soma e multiplicação

---
+++ function.py
@@ -0,0 +1,21 @@
+def soma():
+    nun_01 = 25
+    nun_02 = 94
+
+    resultado = nun_01 * nun_02
+
+    return resultado 
+
+def multiplicarDoisNumeros(nota_01, nota_02):
+    num_01 = nota_01
+    num_02 = nota_02
+
+    resultado = num_01 * num_02
+
+    return print(resultado)
+
+#somarDoisNumeros(25, 94)
+
+multiplicarDoisNumeros(25, 49)
+
+
